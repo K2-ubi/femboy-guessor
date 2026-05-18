f0fd89343dc3e5fb2073e5914bf6dd7666ccc55e
konstasil <konstasil777@gmail.com>
2026-05-18 21:20:42 +0300

    востонавление

---
+++ api/firebase-config.js
@@ -0,0 +1,18 @@
+const { setSecurityHeaders } = require('./_shared');
+
+module.exports = async function handler(req, res) {
+  setSecurityHeaders(res, 'GET, OPTIONS');
+
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  if (req.method !== 'GET') return res.status(405).json({ error: 'GET only' });
+
+  res.json({
+    apiKey: process.env.FIREBASE_API_KEY || '',
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
+    databaseURL: process.env.FIREBASE_DATABASE_URL || '',
+    projectId: process.env.FIREBASE_PROJECT_ID || '',
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
+    appId: process.env.FIREBASE_APP_ID || ''
+  });
+};
